3f9605853b10b9fbe0ffe0822aa7cdd6b14e06f8
OdPavel <98313210+OdPavel@users.noreply.github.com>
2022-03-30 13:44:52 +0300

    correct HW9

--- 9/js/script.js
+++ 9/js/script.js
@@ -3,30 +3,20 @@
 let numOrStr = prompt('input number or string');
 console.log(numOrStr)
 
-// if(numOrStr === null ) {
-//     console.log('вы отменили')
-//  } else if( numOrStr.trim() === '' ) {
-//     console.log('Empty String');
-//  } else if( isNaN( +numOrStr ) ) {
-//     console.log(' number is Ba_NaN')
-// } else {
-//     console.log('OK!')
-// }
-
 // if(numOrStr === null || numOrStr.trim() === '' || isNaN( +numOrStr )) {
 //     console.log('вы сделали ошибку')
 // } else {
 //     console.log('OK!')
 // }
 
-switch (numOrStr) {
-    case numOrStr == null:
+switch (true) {
+    case numOrStr === null:
         console.log('вы отменили');
         break;
-    case numOrStr.trim() == '':
+    case numOrStr.trim() === '':
         console.log('Empty String');
         break;
-    case numOrStr == isNaN( +numOrStr ):
+    case isNaN( +numOrStr ):
         console.log('number is Ba_NaN');
         break;
     default:
