729bc2251845bbda26f1a349ab475103d1e033b3
OdPavel <98313210+OdPavel@users.noreply.github.com>
2022-03-31 11:59:32 +0300

    correct HW11

--- 11/js/script.js
+++ 11/js/script.js
@@ -1,16 +1,76 @@
 'use strict'
 
+let now = new Date();
+let yearOfBirth = +prompt('Привет, у меня есть к тебе несколько вопросов. Какой год твоего рождения?', 1985);
+(yearOfBirth) ? true : alert('Жаль, что ты не захотел ввести свой год рождения');
+let city = prompt('Отлично, подскажи, в каком городе ты живешь?', 'Одессе');
+(city) ? true : alert('Жаль, что ты не захотел ввести свой город');
+let sport = prompt('И последний вопрос, какой твой любимый вид спорта?', 'бокс');
+(sport) ? true : alert('Жаль, что ты не захотел ввести свой любимый вид спорта');
+
+let userYears = now.getFullYear() - yearOfBirth;
+let capitalUkraine = null;
+let capitalEngland = null;
+let capitalFrance = null;
+if(city === 'Киев' || city === 'киев') {
+    capitalUkraine = 'Украине'
+}else if (city === 'Лондон' || city === 'лондон'){
+    capitalEngland = 'Англии';
+}else if (city === 'Париж' || city === 'париж'){
+    capitalFrance = 'Франции';
+}
+let championBox = null;
+let championFootball = null;
+let championJump = null;
+if (sport === 'бокс' || sport === 'Бокс') {
+    championBox = 'Кличко'
+}else if (sport === 'футбол' || sport === 'Футбол'){
+    championFootball = 'Шевченко'
+}else if (sport === 'прыжки' || sport === 'Прыжки'){
+    championJump = 'Бубка'
+}
+
+if (city === 'Киев' || city === 'киев' && sport === 'бокс' || sport === 'Бокс') {
+    alert(`Тебе ${userYears} лет, ты живешь в ${capitalUkraine}, и ты хочешь быть как ${championBox}`);
+} else if (city === 'Лондон' || city === 'лондон' && sport === 'бокс' || sport === 'Бокс'){
+    alert(`Тебе ${userYears} лет, ты живешь в ${capitalEngland}, и ты хочешь быть как ${championBox}`);
+}else if (city === 'Париж' || city === 'париж' && sport === 'бокс' || sport === 'Бокс'){
+    alert(`Тебе ${userYears} лет, ты живешь в ${capitalFrance}, и ты хочешь быть как ${championBox}`);
+}else if (city === 'Киев' || city === 'киев' && sport === 'футбол' || sport === 'Футбол'){
+    alert(`Тебе ${userYears} лет, ты живешь в ${capitalUkraine}, и ты хочешь быть как ${championFootball}`);
+}else if (city === 'Лондон' || city === 'лондон' && sport === 'футбол' || sport === 'Футбол'){
+    alert(`Тебе ${userYears} лет, ты живешь в ${capitalEngland}, и ты хочешь быть как ${championFootball}`);
+}else if (city === 'Париж' || city === 'париж' && sport === 'футбол' || sport === 'Футбол'){
+    alert(`Тебе ${userYears} лет, ты живешь в ${capitalFrance}, и ты хочешь быть как ${championFootball}`);
+}else if (city === 'Киев' || city === 'киев' && sport === 'прыжки' || sport === 'Прыжки'){
+    alert(`Тебе ${userYears} лет, ты живешь в ${capitalUkraine}, и ты хочешь быть как ${championJump}`);
+}else if (city === 'Лондон' || city === 'лондон' && sport === 'прыжки' || sport === 'Прыжки'){
+    alert(`Тебе ${userYears} лет, ты живешь в ${capitalEngland}, и ты хочешь быть как ${championJump}`);
+}else if (city === 'Париж' || city === 'париж' && sport === 'прыжки' || sport === 'Прыжки'){
+    alert(`Тебе ${userYears} лет, ты живешь в ${capitalFrance}, и ты хочешь быть как ${championJump}`);
+}else{
+    alert(`Тебе ${userYears} лет, ты живешь в ${city}, и ты хочешь заниматься ${sport}`);
+}
+
+
+
+
+
+
+
+
+
 // let year = +prompt('Привет, у меня есть к тебе несколько вопросов. Какой год твоего рождения?', 1985);
 // if (year > 1945 || year < 2020){
 //     let city = prompt('Отлично, подскажи, в каком городе ты живешь?', 'Odessa');
-    // if (" "){
-    //     let sport = prompt('И последний вопрос, какой твой любимый вид спорта?', 'boxing');
-    //     if (" "){}else{
-    //         alert('Жаль, что ты не захотел ввести свой любимый вид спорта')
-    //     }
-    // }else{
-    //     alert('Жаль, что ты не захотел ввести свой город')
-    // }
+// if (" "){
+//     let sport = prompt('И последний вопрос, какой твой любимый вид спорта?', 'boxing');
+//     if (" "){}else{
+//         alert('Жаль, что ты не захотел ввести свой любимый вид спорта')
+//     }
+// }else{
+//     alert('Жаль, что ты не захотел ввести свой город')
+// }
 // }else{
 //     alert('Жаль, что ты не захотел ввести свой год рождения')
 // }
@@ -22,31 +82,3 @@
 // }else{
 //     alert('Жаль, что ты не захотел ввести свой год рождения')
 // }
-
-let now = new Date();
-let yearOfBirth = +prompt('Привет, у меня есть к тебе несколько вопросов. Какой год твоего рождения?', 1985);
-(yearOfBirth) ? true : alert('Жаль, что ты не захотел ввести свой год рождения');
-let city = prompt('Отлично, подскажи, в каком городе ты живешь?', 'Odessa');
-(city) ? true : alert('Жаль, что ты не захотел ввести свой город');
-let sport = prompt('И последний вопрос, какой твой любимый вид спорта?', 'boxing');
-(sport) ? true : alert('Жаль, что ты не захотел ввести свой любимый вид спорта');
-
-let userYears = now.getFullYear() - yearOfBirth;
-
-if(city === 'Киев' && sport === 'бокс') {
-    alert(`Тебе ${userYears} лет, ты живешь в столице Украины, ты хочешь стать боксером как Кличко`);
-}else if (city === 'Лондон'){
-    let capitalEngland = 'Англии';
-}else if (city === 'Париж'){
-    let capitalFrance = 'Франции';
-    console.log(capitalFrance)
-}
-if (sport === 'бокс') {
-    let championBox = 'Кличко'
-}else if (sport === 'футбол'){
-    let championFootball = 'Шевченко'
-}else if (sport === 'прыжки'){
-    let championJump = 'Бубка'
-}
-console.log(city)
-// // alert(`Тебе ${}`)
