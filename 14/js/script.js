74b41672abbb25d224a1cc81db23b9600d664034
OdPavel <98313210+OdPavel@users.noreply.github.com>
2022-04-07 10:16:46 +0300

    correct 14

--- 14/js/script.js
+++ 14/js/script.js
@@ -5,7 +5,7 @@ let num = true;
 
 if (simpleNum == 1){
     console.log(false)
-}else {
+}else if(simpleNum > 1){
     for (let i = 2; i < simpleNum; i++) {
         if (simpleNum % i === 0) {
       num = false;
