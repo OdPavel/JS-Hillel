aba1908a36b2cb7e7303e9d4300a4885a12bb7df
OdPavel <98313210+OdPavel@users.noreply.github.com>
2022-04-11 19:40:12 +0300

    add HW19

---
+++ 18/js/script.js
@@ -0,0 +1,6 @@
+'use strict'
+
+// const arr = [1,2,3,-1,-2,-3];
+function myArr(arr) {
+
+}
---
+++ 19/js/script.js
@@ -0,0 +1,89 @@
+'use strict'
+
+let result = null;
+let num = null;
+let num2 = null;
+
+function mainFunction(callback) {
+    do{
+        num = +prompt('Enter number');
+        num2 = +prompt('Enter num2');
+    }
+    while (isNaN(num) || isNaN(num2) || num === null || num2 === null);
+    callback(num, num2);
+}
+
+function exponentiation(num, num2) {
+    result = Math.pow(num, num2);
+    return
+}
+
+function multiplay(num, num2) {
+    result = num * num2;
+    return
+}
+
+function division(num, num2) {
+    result = num / num2;
+    return
+}
+
+function modulo(num, num2) {
+    result = num % num2;
+    return
+}
+
+mainFunction(exponentiation);
+// mainFunction(multiplay);
+// mainFunction(division);
+// mainFunction(modulo);
+console.log(result);
+
+
+// let result = null;
+// let num;
+// let num2;
+// let znak;
+//
+// function mainFunction(callback) {
+//     num = +prompt('Enter number');
+//     num2 = +prompt('Enter num2');
+//     znak = prompt('Wha do You want to do: ^, *, / or %');
+//     callback(num, num2, znak);
+// }
+//
+// function exponentiation(num, num2) {
+//     result = Math.pow(num, num2);
+//     return
+// }
+//
+// function multiplay(num, num2) {
+//     result = num * num2;
+//     return
+// }
+//
+// function division(num, num2) {
+//     result = num / num2;
+//     return
+// }
+//
+// function modulo(num, num2) {
+//     result = num % num2;
+//     return
+// }
+//
+// mainFunction(exponentiation);
+// // mainFunction(multiplay);
+// // mainFunction(division);
+// // mainFunction(modulo);
+// console.log(result);
+//
+// if (znak === '^'){
+//     exponentiation();
+// }else if(znak === '*'){
+//     multiplay();
+// }else if(znak === '/'){
+//     division();
+// }else if(znak === '%'){
+//     modulo();
+// }
