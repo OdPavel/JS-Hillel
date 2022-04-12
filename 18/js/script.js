d6bbc2194525b3dd6d2a1905d259c12722169305
OdPavel <98313210+OdPavel@users.noreply.github.com>
2022-04-12 11:04:47 +0300

    add HW18

--- 18/js/script.js
+++ 18/js/script.js
@@ -1,6 +1,26 @@
 'use strict'
 
-// const arr = [1,2,3,-1,-2,-3];
-function myArr(arr) {
+const arr = [1, 2, 3, -1, -2, -3];
 
+function myArr(a) {
+    const exampleArr = [];
+    if (arr.length === 0) {
+        console.log('Arr is example');
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            exampleArr.push(arr[i]);
+        }
+    }
+    if (exampleArr.length > 0) {
+        // console.log(true)
+        return;
+    } else {
+        // console.log(false)
+        return null;
+    }
 }
+
+myArr(arr);
+
+
