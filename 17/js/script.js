63c90d9be8d389514b978b6c4df6409eccc7861e
OdPavel <98313210+OdPavel@users.noreply.github.com>
2022-04-13 13:30:37 +0300

    add HW17

--- 17/js/script.js
+++ 17/js/script.js
@@ -1,18 +1,24 @@
 'use strict'
 
-// function padString(str, num, sym, bool = true) {
-//     // if (typeof str !== 'string') {
-//     //     return 'error string'
-//     // } else if (typeof sym !== 'string' && sym.length === 1) {
-//     //     return 'error symbol'
-//     // } else if (typeof num !== 'number') {
-//     //     return 'error number'
-//
-// let a = padString();
-//     return a;
-// }
-//
-// padString('hello', 8, '*');
-// console.log(a);
+function padString(str, num, sym, bool = true) {
+    if(typeof str !== 'string' || str === ''){
+        return console.log('string error');
+    }
+    if(typeof num !== 'number' || num === ''){
+        return console.log('number error');
+    }
+    if(typeof sym !== 'string' || sym === '' || sym.length !== 1){
+        return 'symbol error';
+    }
+    if (num < str.length){
+        return str.substr(0, num);
+    }
+    if (bool) {
+        return str.padEnd(num, sym)
+    }
+    if (bool === false) {
+        return str.padStart(num, sym)
+    }
+}
 
-
+console.log(padString('hello', 20, '*'))
