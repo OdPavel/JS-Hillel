5851bcd634debf5ffae69108005e7575b42b8a47
OdPavel <98313210+OdPavel@users.noreply.github.com>
2022-05-13 19:26:13 +0300

    add HW29

---
+++ 29/js/script.js
@@ -0,0 +1,27 @@
+'use strict'
+
+    let ladder = {
+        step: 0,
+        up: function() {
+            this.step++;
+            return this;
+        },
+        down: function() {
+            this.step--;
+            return this
+        },
+        showStep: function() { // показывает текущую ступеньку
+            console.log(this.step);
+            return this
+        }
+    };
+
+    // ladder.up();
+    // ladder.up();
+    // ladder.down();
+    // ladder.showStep();
+
+
+ladder.up().up().down().showStep();
+
+
