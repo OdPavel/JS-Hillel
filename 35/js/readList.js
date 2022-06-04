1e6425d36a66c9903dac183f390defb7c2e77c9a
OdPavel <98313210+OdPavel@users.noreply.github.com>
2022-06-04 17:37:10 +0300

    correct HW35

--- 35/js/readList.js
+++ 35/js/readList.js
@@ -3,20 +3,21 @@
 void function () {
 
     const formSelector = '#form';
-    const form = document.querySelector(formSelector);
+
     const readList = () => {
-        let body1 = document.querySelector('#someBody');
-        if (localStorage.getItem(formSelector) === null) {
+        const searchInputs = localStorage.getItem(formSelector);
+        const body1 = document.querySelector('#someBody');
+        if (searchInputs === null) {
             const h1 = document.createElement('h1');
-            h1.innerHTML = 'Local Storage Empty';
+            h1.innerHTML = 'Local Storage is EMPTY';
             body1.append(h1)
             return
         }
 
-        let ul = document.createElement('ul');
+        const ul = document.createElement('ul');
         body1.append(ul);
 
-        const data = JSON.parse(localStorage.getItem(formSelector));
+        const data = JSON.parse(searchInputs);
         for (const item in data) {
             const li = document.createElement('li')
             li.textContent = `${item}  =  ${data[item]}`;
